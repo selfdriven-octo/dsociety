ff9566eaaa43d79455a09e266c384821614a6134
selfdriven-foundation <mark.byers@selfdriven.foundation>
2026-01-23 11:34:00 +1100

    Update dsociety.entityos.learn.ssi.keri-2.0.0.js

--- docs/js/dsociety.entityos.learn.ssi.keri-2.0.0.js
+++ docs/js/dsociety.entityos.learn.ssi.keri-2.0.0.js
@@ -151,7 +151,7 @@ eos.add(
             learnSSIKERIView.add(
 			[
 				'<div style="background-color:rgba(0,0,0,0.7); border-radius: 6px; padding:16px;" class="w-md-100 mt-4 mb-4">',
-					'<h4 class="fw-bold mb-3 mt-1">Step 2 | DID based on dSociety SSI Method Specification</h4>',
+					'<h4 class="fw-bold mb-3 mt-1">Step 2 | AID</h4>',
 					'<div class="" style="color:#e8d5cf;">Autonomic ID (AID) | Public Key | Base64</div>',
 					'<div style="font-family: PT Mono, monospace; font-size: 1rem; color:#baadab; word-break: break-all;" class="mb-1">',
 						ssiKERIAID,
@@ -164,6 +164,27 @@ eos.add(
 					'</div>',
                 '</div>'
 			]);
+
+			learnSSIKERIView.add(
+			[
+				'<div style="background-color:rgba(0,0,0,0.7); border-radius: 6px; padding:16px;" class="w-md-100 mt-2 mb-4">',
+					'<h4 class="fw-bold mb-3 mt-1">Step 3 | Inception | Key Event Ledger</h4>',
+					'<div class="d-none mb-1">',
+						'<textarea id="url-text" class="form-control entityos-text w-100 border border-info"',
+                            ' style="height:180px;" data-scope="learn-ssi-keri"',
+                            ' data-context="data-to-sign"></textarea>',
+                    '</div>',
+                '</div>',
+				'<button type="button" class="btn btn-sm btn-outline-primary text-light entityos-click mb-2"',
+					' data-controller="learn-ssi-keri-create-event-inception" style="width: 200px;">',
+					'Create Inception Event',
+				'</button>',
+				'<div id="learn-ssi-keri-create-event-inception-view"></div>'
+			]);
+
+			/*
+
+			Later as ACDC.
 
             // Create proof - hash the message and then sign/encrypt with aid
 
@@ -184,6 +205,7 @@ eos.add(
 				'<div id="learn-ssi-keri-create-signature-view"></div>',
 				'<div class="mt-4">In console run entityos._util.controller.invoke("learn-ssi-keri-create-event-[inception,rotation,delegation]")</div>'
 			]);
+			*/
 
 			learnSSIKERIView.render('#learn-view')
 		}
@@ -430,7 +452,9 @@ eos.add(
 				value: state
 			});
 
-			/*let learnSSIView = eos.view();
+			const keriInceptionEventFormatted = JSON.stringify(keriInceptionEvent, null, 2)
+
+			let learnSSIView = eos.view();
 
 			learnSSIView.add(
 			[
@@ -438,18 +462,17 @@ eos.add(
 					'<h4 class="fw-bold mb-3 mt-1">Step 5 | ', cryptoCurve, ' Private Key Signature of SHA256 Hash of the Data</h4>',
 					'<div class="" style="color:#e8d5cf;">KERI Inception Event</div>',
 					'<div style="font-family: PT Mono, monospace; font-size: 1rem; color:#baadab; word-break: break-all;" class="mb-1">',
-						keriInceptionEvent,
+						keriInceptionEventFormatted,
 					'</div>',
                 '</div>',
 				'<button type="button" class="btn btn-sm btn-outline-primary text-light entityos-click mb-2"',
-					' data-controller="learn-ssi-keri-verify-signature" style="width: 200px;">',
+					' data-controller="learn-ssi-keri-create-event-rotation-view" style="width: 200px;">',
 					'Rotate',
 				'</button>',
-				'<div id="learn-ssi-keri-verify-signature-view"></div>'
+				'<div id="learn-ssi-keri-create-event-rotation-view"></div>'
 			]);
 
-			learnSSIView.render('#learn-ssi-keri-create-signature-view')
-			*/
+			learnSSIView.render('#learn-ssi-keri-create-event-inception-view');
 		}
 	},
 	{
@@ -593,26 +616,27 @@ eos.add(
 
 			console.log(state)
 
-			/*let learnSSIView = eos.view();
+			const keriRotationEventFormatted = JSON.stringify(keriRotationEvent, null, 2)
+
+			let learnSSIView = eos.view();
 
 			learnSSIView.add(
 			[
 				'<div style="background-color:rgba(0,0,0,0.7); border-radius: 6px; padding:16px;" class="w-md-100 mt-2 mb-4">',
-					'<h4 class="fw-bold mb-3 mt-1">Step 5 | ', cryptoCurve, ' Private Key Signature of SHA256 Hash of the Data</h4>',
-					'<div class="" style="color:#e8d5cf;">KERI Inception Event</div>',
+					'<h4 class="fw-bold mb-3 mt-1">Step 6 | Rotation </h4>',
+					'<div class="" style="color:#e8d5cf;">KERI Rotation Event</div>',
 					'<div style="font-family: PT Mono, monospace; font-size: 1rem; color:#baadab; word-break: break-all;" class="mb-1">',
-						keriInceptionEvent,
+						keriRotationEventFormatted,
 					'</div>',
                 '</div>',
 				'<button type="button" class="btn btn-sm btn-outline-primary text-light entityos-click mb-2"',
-					' data-controller="learn-ssi-keri-verify-signature" style="width: 200px;">',
+					' data-controller="learn-ssi-keri-create-event-rotation-view" style="width: 200px;">',
 					'Rotate',
 				'</button>',
-				'<div id="learn-ssi-keri-verify-signature-view"></div>'
+				'<div id="learn-ssi-keri-create-acdc-view"></div>'
 			]);
 
-			learnSSIView.render('#learn-ssi-keri-create-signature-view')
-			*/
+			learnSSIView.render('#learn-ssi-keri-create-event-rotation-view');
 		}
 	},
 	{
@@ -920,6 +944,28 @@ eos.add(
 				context: 'state',
 				value: state
 			});
+
+			const acdcFormatted = JSON.stringify(acdc, null, 2)
+
+			let learnSSIView = eos.view();
+
+			learnSSIView.add(
+			[
+				'<div style="background-color:rgba(0,0,0,0.7); border-radius: 6px; padding:16px;" class="w-md-100 mt-2 mb-4">',
+					'<h4 class="fw-bold mb-3 mt-1">Step 7 | Authentic Chained Data Container (ACDC)</h4>',
+					'<div class="" style="color:#e8d5cf;">KERI ACDC</div>',
+					'<div style="font-family: PT Mono, monospace; font-size: 1rem; color:#baadab; word-break: break-all;" class="mb-1">',
+						acdcFormatted,
+					'</div>',
+                '</div>',
+				'<button type="button" class="d-none btn btn-sm btn-outline-primary text-light entityos-click mb-2"',
+					' data-controller="learn-ssi-keri-xxx-view" style="width: 200px;">',
+					'xxx',
+				'</button>',
+				'<div id="learn-ssi-keri-xxx-view"></div>'
+			]);
+
+			learnSSIView.render('#learn-ssi-keri-create-event-acdc-view');
 		}
 	}
 ]);
